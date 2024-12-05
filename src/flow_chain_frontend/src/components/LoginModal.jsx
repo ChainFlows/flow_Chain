67f2d483b4283351bd1d73ea28f251fb5b6e04d2
45jerryjoseph <45jerryjoseph@gmail.com>
2024-12-05 22:38:12 +0300

    some frontend slight changes

--- src/flow_chain_frontend/src/components/LoginModal.jsx
+++ src/flow_chain_frontend/src/components/LoginModal.jsx
@@ -7,8 +7,7 @@ export default function LoginModal({ isOpen, onClose }) {
   const navigate = useNavigate();
 
   const roles = [
-    ["Admin", "Warehouse Manager", "Guest"],
-    ["Client", "Supplier", "Driver"],
+    ['Client', 'Supplier', 'Driver']
   ];
   const handleLogin = () => {
     if (selectedRole) {
